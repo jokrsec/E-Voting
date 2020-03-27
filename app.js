58713e04eee81461c5cde943264228cccc364a66
jokr <loganalex007@gmail.com>
2020-03-27 08:56:50 -0400

    web design, users database, level 2 client authentication

---
+++ app.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const encrypt = require('mongoose-encryption');
+const cool = require('cool-ascii-faces');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.get('/cool', (req, res) => res.send(cool()));
+
+
+mongoose.connect("mongodb+srv://jokr:Hemanth007@cluster0-ymhul.mongodb.net/evotingUsersDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login", {validation: "normal"});
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/result", function(req, res){
+    res.render("result");
+});
+
+app.post("/register", function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    new User({
+        email: email,
+        password: password
+    }).save();
+    console.log("Registered New User");
+    res.render("submit");
+
+});
+
+app.post("/login", function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email: email}, function(err, foundUser){
+        if(foundUser==null){
+            console.log("User Doesn't Exist");
+            res.render("login", {validation: "nouser"});
+        }
+        else{
+            if(foundUser.password === password){
+                console.log("User LoggedIn");
+                res.render("submit");
+            }
+            else{
+                console.log("Wrong Password");
+                res.render("login", {validation: "wrong"});
+            }
+        }   
+    });
+});
+
+
+let port = process.env.PORT;
+if(port == null || port == "")
+    port = 3000;
+
+
+app.listen(port, function(){
+    console.log("listening on port "+port);
+});
+
+
